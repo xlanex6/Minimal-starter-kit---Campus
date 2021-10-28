9dd5b5680be0daabffb9b347804ff5625cde92ec
Alex Duval <alexduval71@gmail.com>
2021-10-28 20:05:47 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Vue 3 + Vite MINIMAL Starter KIT Campus
+
+## Recommended IDE Setup
+
+- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>{{ msg }}</h1>
+
+  <button
+    type="button"
+    @click="count++"
+  >count is: {{ count }}</button>
+</template>
+
+<script>
+export default {
+  props: ["msg"],
+  data() {
+    return {
+      count: 1,
+    };
+  },
+};
+</script>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+</style>
